62330db21058f891d940b75c3fd8e48d741f2bee
Shohrux99 <b.shohrux@mail.ru>
2022-02-21 14:59:41 +0500

    .

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
-    path('', ainclude('client.urls')),
+    path('', include('client.urls')),
 ]
