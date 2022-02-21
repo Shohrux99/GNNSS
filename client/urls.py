2eef6b72953b10179c2e80f2acecf6db54f5cf78
Shohrux99 <b.shohrux@mail.ru>
2022-02-21 17:01:40 +0500

    adm

--- client/urls.py
+++ client/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, CreateViews
 urlpatterns = [
     path('',IndexView,name='index'),
+    path('create',CreateViews,name='create'),
+
 ]
--- client/views.py
+++ client/views.py
@@ -5,3 +5,8 @@ def IndexView(request):
 
 
     return render(request,'main/index.html')
+
+def CreateViews(request):
+
+
+    return render(request,'main/create.html')
